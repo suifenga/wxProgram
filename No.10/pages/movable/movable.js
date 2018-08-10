afc37c43038bbb64124bc57c8eca21108a08f931
394498036@qq.com <394498036@qq.com>
2018-08-10 22:01:47 +0800

    1.view 2.scroll-view 3.movable 4.swiper 样例源码

---
+++ No.10/pages/movable/movable.js
@@ -0,0 +1,11 @@
+//movable.js
+//获取应用实例
+
+Page({
+  onchange:function(){
+    console.log("在移动。。");
+  },
+  onscale:function(){
+    console.log("在缩放")
+  }
+})
---
+++ No.10/pages/scroll-view/scroll-view.js
@@ -0,0 +1,15 @@
+//scroll-view.js
+//获取应用实例
+
+Page({
+  scrolltoupper:function(){
+      console.log("滚动到顶部");
+  },
+  scrolltolower:function(){
+      console.log("滚动到底部");
+  },
+  scroll:function(){
+    console.log("滚动中。。。");
+  }
+ 
+})
